013061e16e299f38328fda13033af2761eac7c75
Andrey Zhizhov <andrey_learning@mail.ru>
2020-06-23 21:24:02 +0500

    добавил визуал для клиента и сервера

--- client/src/main/java/view/Controller.java
+++ client/src/main/java/view/Controller.java
@@ -1,4 +1,10 @@
 package view;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void exitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
